4b131c130820ea8bb06c13f1463fb809c29ed1bb
Chris Cooper <bloodwolf444@gmail.com>
2021-06-03 14:53:44 -0700

    Added a ComicBook Detail() view.

--- Project9-Comic-Book/Controllers/ComicBookController.cs
+++ Project9-Comic-Book/Controllers/ComicBookController.cs
@@ -11,13 +11,7 @@ namespace Project9_Comic_Book.Controllers
         public ActionResult Detail()
         {
 
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-               
-            }
-
-            return Content("Hello from the console.");
+            return View();
 
            
 

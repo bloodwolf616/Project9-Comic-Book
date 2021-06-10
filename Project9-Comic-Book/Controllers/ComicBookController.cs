ae0a60a92bd79572cb23f08ee1e82d6d0c1a88f9
Chris Cooper <bloodwolf444@gmail.com>
2021-06-10 15:02:40 -0700

    Updated the Detail view and added an image folder.

--- Project9-Comic-Book/Controllers/ComicBookController.cs
+++ Project9-Comic-Book/Controllers/ComicBookController.cs
@@ -16,6 +16,7 @@ namespace Project9_Comic_Book.Controllers
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
                 DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Favorite = false,
                 Artists = new Artist[]
                 {
                     new Artist() {Name = "Dan Slott", Role = "Script"},
@@ -24,7 +25,7 @@ namespace Project9_Comic_Book.Controllers
                     new Artist() {Name = "Edgar Delgado", Role = "Colors"},
                     new Artist() {Name = "Chris Eliopoulos", Role = "Letters"},
                 } 
-            
+               
             
             };
 

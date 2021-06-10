527b163737cc6e71ac1bb15c6f1c4bf7a7025311
Chris Cooper <bloodwolf444@gmail.com>
2021-06-09 20:52:20 -0700

    added a second Controller/View called RandomPage/Index. also added two models called ComicBook and Artists to hold our data.

---
+++ Project9-Comic-Book/Controllers/RandomPageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project9_Comic_Book.Controllers
+{
+    public class RandomPageController : Controller
+    {
+        // GET: RandomPage
+        public ActionResult Index()
+        {
+            ViewBag.GroceryList = new string[] { "Apples", "Oranges", "Bananas" };
+            return View();
+        }
+    }
+}

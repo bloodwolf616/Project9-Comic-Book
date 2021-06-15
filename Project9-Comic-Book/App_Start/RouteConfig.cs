dcd92ee10586e2dd8ce82f9940d67b3f42d6726a
Chris Cooper <bloodwolf444@gmail.com>
2021-06-15 16:16:12 -0700

    Upadated the defualt page.

--- Project9-Comic-Book/App_Start/RouteConfig.cs
+++ Project9-Comic-Book/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Project9_Comic_Book
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ComicBook", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
